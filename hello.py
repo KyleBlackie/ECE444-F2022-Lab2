e49a403bad3722a385fc43dfea87a3111c6b1504
Kyle Blackie <kyle.blackie@mail.utoronto.ca>
2022-09-28 11:24:27 -0400

    Finished Activity 4

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import EmailField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -14,19 +14,27 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>Hello, {}!</h1>'.format(name)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    email = session.get('email')
+    if not _check_valid_uoft_email(email):
+        email = ''
+    return render_template('index.html', form=form, name=session.get('name'), email=email)
+
+def _check_valid_uoft_email(email):
+    return email is not None and 'utoronto' in email
+        
